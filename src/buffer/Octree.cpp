52f5ab98f5c78325e32bc2bae74b7f37982f8527
jon0 <jono4728@gmail.com>
2014-03-28 21:14:49 +1300

    better rt

--- src/buffer/Octree.cpp
+++ src/buffer/Octree.cpp
@@ -5,9 +5,30 @@
  *      Author: remnanjona
  */
 
+#include <stdlib.h>
+
 #include "Octree.h"
 
 namespace std {
+
+struct uc_rgba {
+	unsigned char r, g, b, a;
+};
+
+void makeSphere(uc_rgba *image_data, int levels, glm::vec3 mid, float radius) {
+	for (int i = 0; i < levels; ++i) {
+		for (int j = 0; j < levels; ++j) {
+			for (int k = 0; k < levels; ++k) {
+				float dist = glm::distance(mid, glm::vec3(i,j,k));
+				if (dist < radius) {
+					glm::vec3 n =  glm::vec3(i,j,k) - mid;
+					if (glm::length(n) > 0) n = glm::normalize(n);
+					image_data[i + j * levels + k * levels * levels] = {128 + 128*n.x, 128 + 128*n.y, 128 + 128*n.y, 255};
+				}
+			}
+		}
+	}
+}
 
 Octree::Octree(unsigned int l) {
 	levels = l;
@@ -19,8 +40,20 @@ Octree::Octree(unsigned int l) {
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
 
+	uc_rgba *image_data = new uc_rgba [levels*levels*levels]();
+
+	for (int s = 0; s < 20; ++s) {
+		glm::vec3 mid = glm::vec3(levels/4.0f + rand()%levels/2.0f,
+				levels/4.0f + rand()%levels/2.0f,
+				levels/4.0f + rand()%levels/2.0f);
+
+		float radius = rand() % levels/10.0f;
+		makeSphere(image_data, levels, mid, radius);
+	}
+
+
 	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA8, levels, levels, levels, 0, GL_RGBA,
-	             GL_FLOAT, 0);
+			GL_UNSIGNED_BYTE, image_data);
 
 	//glGenerateMipmap(GL_TEXTURE_3D);
 
